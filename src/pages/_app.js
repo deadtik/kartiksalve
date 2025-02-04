ab9429eb1942cf4bdf1e8e2a09282fbac17e0d77
Kartik Salve <76260440+deadtik@users.noreply.github.com>
2025-02-04 18:03:34 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,41 +1,42 @@
-import Footer from '@/components/Footer'
-import NavBar from '@/components/NavBar'
-import '@/styles/globals.css'
-import { AnimatePresence } from 'framer-motion'
-import {Montserrat} from "next/font/google"
-import { useRouter } from 'next/router'
-import Head from 'next/head'
+import Footer from '@/components/Footer';
+import NavBar from '@/components/NavBar';
+import '@/styles/globals.css';
+import { AnimatePresence } from 'framer-motion';
+import { Montserrat } from 'next/font/google';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
-/**
- * Montserrat font configuration.
- *
- * @type {Object}
- * @property {string[]} subsets - The subsets of the font.
- * @property {string} variable - The CSS variable for the font.
- */
-
+// Montserrat font configuration
 const montserrat = Montserrat({
   subsets: ['latin'],
-  variable: "--font-mont"
-})
+  variable: '--font-mont',
+});
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-  return(
-   <>
+  const [mounted, setMounted] = useState(false);
 
-    <Head>
-      <div>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" /> </div>
-    </Head>
-    <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen `}>
-      <NavBar />
-      <AnimatePresence>
-      <Component key={router.asPath} {...pageProps} />
-      </AnimatePresence>
-      <Footer />
-    </main>
-   </>
-  ) 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
+        <NavBar />
+        {mounted && (
+          <AnimatePresence mode="wait">
+            <Component key={router.asPath} {...pageProps} />
+          </AnimatePresence>
+        )}
+        <Footer />
+      </main>
+    </>
+  );
 }
